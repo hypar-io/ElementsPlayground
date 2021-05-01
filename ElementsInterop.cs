75a75794aeaa1f65517feb2f43a64ef4de43ef42
Ian Keough <ian@hypar.io>
2021-05-01 13:30:56 -0700

    Add a wrapper.

---
+++ ElementsInterop.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Elements;
+using Elements.Geometry;
+using Elements.Geometry.Interfaces;
+using Elements.Serialization.glTF;
+using Elements.Spatial;
+using Microsoft.JSInterop;
+
+namespace ElementsWasm
+{
+    public static class ElementsInterop
+    {
+        [JSInvokable]
+        public static Task<Line[]> CreateGrid2dAsync(int uCount, int vCount)
+        {
+            var u = new Grid1d();
+            u.DivideByCount(uCount);
+            var v = new Grid1d();
+            v.DivideByCount(vCount);
+            var g = new Grid2d(u, v);
+            // We need to send out serializable stuff.
+            return Task.FromResult(g.GetCellSeparators(GridDirection.U).Concat(g.GetCellSeparators(GridDirection.V)).Cast<Line>().ToArray());
+        }
+    }
+}
